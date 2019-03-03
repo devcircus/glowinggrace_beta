a35574d61f0fff5f8637a1da03186913606ff05b
Clayton Stone <devcircus@outlook.com>
2019-03-02 21:44:41 -0600

    Force Https on all routes.

---
+++ app/Providers/HttpsServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class HttpsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (config('app.env') === 'production') {
+            URL::forceSchema('https');
+        }
+    }
+}
