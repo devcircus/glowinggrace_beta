96450c484ed8b673d6faa4d44bbeff6a8475f712
Clayton Stone <devcircus@outlook.com>
2019-03-07 11:13:01 -0600

    Add favicon.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,6 +16,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('page_css')
 </head>
+
 <body class="bg-blue-lightest">
     <div id="base-app">
         @yield('content')
@@ -23,4 +26,5 @@
     <script src="{{ asset('js/main.js') }}"></script>
     @yield('page_scripts');
 </body>
+
 </html>
